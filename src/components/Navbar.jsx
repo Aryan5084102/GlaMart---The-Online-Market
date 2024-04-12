207bd1dd00e8efaa58f8ac4b422707139facda3d
Aryan5084102 <aryanverma5084102@gmail.com>
2024-04-12 17:02:18 +0530

    update logo path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ import Badge from '@mui/material/Badge';
 
 
 function Navbar() {
+
   const { setSearchTerm } = useContext(searchContext)
   const [userData, setUserData] = useState(JSON.parse(sessionStorage.getItem("userData")) || [])
   console.log(userData, "userData value")
@@ -42,7 +43,7 @@ function Navbar() {
       />
       <div className='flex bg-[#D7E4C0] justify-between items-center px-4 py-2 sticky top-0 left-0 z-10 '>
         <Link to="/" className='flex items-center '>
-          <div className='w-20 h-16 mr-1 '> <img src='../favicon/favicon.png' alt='Logo' /> </div>
+          <div className='w-20 h-16 mr-1 '> <img src='../src/favicon.png' alt='Logo' /> </div>
           <div className='text-3xl font-extrabold hover:text-[#0B7A74] cursor-text '> <a href='#'>GlaMart</a> </div>
         </Link>
         <div className='flex items-center w-1/3 h-10 bg-white rounded-lg'>
